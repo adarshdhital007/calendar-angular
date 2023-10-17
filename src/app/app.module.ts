fc4e23d780713da28d7b0707d3cb781b23e1bce8
Adarsh Dhital Khatri <adarsh7466@gmail.com>
2023-10-17 22:26:59 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { EventDetailsComponent } from './event-details/event-details.component';
 
@@ -10,7 +10,7 @@ import { EventDetailsComponent } from './event-details/event-details.component';
     EventDetailsComponent
   ],
   imports: [
-    BrowserModule,FormsModule
+    BrowserModule,FormsModule,ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
