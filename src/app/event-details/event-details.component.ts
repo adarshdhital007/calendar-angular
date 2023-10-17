0cacb0b9274f9875c33767db695645e66b69e6eb
Adarsh Dhital Khatri <adarsh7466@gmail.com>
2023-10-17 22:26:41 +0530

    Added small minor stuff

--- src/app/event-details/event-details.component.ts
+++ src/app/event-details/event-details.component.ts
@@ -6,6 +6,6 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./event-details.component.css']
 })
 export class EventDetailsComponent {
-  @Input() event: { title: string, description: string } = { title: '', description: '' }; // Provide default values
+  @Input() event: { title: string, description: string } | null = { title: '', description: '' };
   @Output() close = new EventEmitter<void>();
 }
