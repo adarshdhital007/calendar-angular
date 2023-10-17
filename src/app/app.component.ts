1750c14b5acf6d5b981929e61db9d406c85fd933
Adarsh Dhital Khatri <adarsh7466@gmail.com>
2023-10-17 22:28:13 +0530

    Added Previous Month Days and Next Days for Next Month

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,96 +1,141 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormGroup, FormBuilder } from '@angular/forms';
+
+interface CalendarCell {
+  day: number;
+  isPreviousMonth: boolean;
+  isNextMonth: boolean;
+  event?: { title: string; description: string };
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'calendar-angular';
   selectedMonth: Date = new Date();
   selectedMonthIndex: number = new Date().getMonth();
-  selectedEvent: { title: string, description: string } | null = null;
+  selectedEvent: { title: string; description: string } | null = null;
 
   months: string[] = [
-    'January', 'February', 'March', 'April', 'May', 'June',
-    'July', 'August', 'September', 'October', 'November', 'December'
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
   ];
 
   weekDays: string[] = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  previousMonthDays: number = 0;
+
+  events: { date: string; title: string; description: string }[] = [
+    {
+      date: '2023-10-13',
+      title: 'Taylor Swift Movie',
+      description:
+        'The movie was recently released depicting and showing the ERAS tour',
+    },
+    {
+      date: '2023-10-14',
+      title: 'Logan vs Dillon',
+      description:
+        'This is the latest boxing fight where two legends are facing each other.',
+    },
+    { date: '2023-11-29', title: 'ETE 1', description: 'Penetration Testing' },
+    {
+      date: '2023-12-01',
+      title: 'ETE 2',
+      description: 'Securing Computing Systems',
+    },
+  ];
+  selectedEventMain: { title: string; description: string } | null = null;
+  eventDetailsVisible = false;
+
+  openEventDetails(event: { title: string; description: string }) {
+    this.selectedEventMain = event;
+    this.eventDetailsVisible = true;
+  }
+
+  getEventsForDay(day: number): { title: string; description: string }[] {
+    const dateKey = this.getDateKey(day);
+    return this.events.filter((event) => event.date === dateKey);
+  }
 
   getDateKey(day: number): string {
     const year = this.selectedMonth.getFullYear();
     const month = this.selectedMonth.getMonth() + 1;
-    return `${year}-${month.toString().padStart(2, '0')}-${day.toString().padStart(2, '0')}`;
+    return `${year}-${month.toString().padStart(2, '0')}-${day
+      .toString()
+      .padStart(2, '0')}`;
   }
-  // Rough Events
-  events: { [key: string]: { title: string, description: string }[] } = {
-    '2023-10-14': [
-      { title: 'Logan vs Dillon', description: 'This is the latest boxing fight where two legends are facing each other.' },
-    ],
-    '2023-10-13': [
-      { title: 'Taylor Swift Movie', description: 'The movie was recently released depicting and showing the ERAS tour' },
-    ],
-    '2023-11-29': [
-      { title: 'ETE', description: 'INT 244' },
-    ],
-    '2023-12-01': [
-      { title: 'ETE 2', description: 'INT 245' },
-    ],
-  };
+
   onMonthChange() {
-    this.selectedMonth = new Date(this.selectedMonth.getFullYear(), this.selectedMonthIndex, 1);
+    this.selectedMonth = new Date(
+      this.selectedMonth.getFullYear(),
+      this.selectedMonthIndex,
+      1
+    );
   }
-  get calendar(): number[][] {
+
+  get calendar(): CalendarCell[][] {
     const year = this.selectedMonth.getFullYear();
     const month = this.selectedMonth.getMonth();
     const firstDay = new Date(year, month, 1).getDay();
     const daysInMonth = new Date(year, month + 1, 0).getDate();
-    const lastDay = new Date(year, month, daysInMonth).getDay();
 
-    const calendar: number[][] = [];
-    let currentWeek: number[] = [];
+    const calendar: CalendarCell[][] = [];
+    let currentWeek: CalendarCell[] = [];
 
-    // Add previous month's days
+    this.previousMonthDays = new Date(year, month, 0).getDate();
     for (let i = firstDay; i > 0; i--) {
-      currentWeek.push(0);
+      currentWeek.push({
+        day: this.previousMonthDays - i + 1,
+        isPreviousMonth: true,
+        isNextMonth: false,
+        event: undefined,
+      });
     }
 
-    // Add current month's days
     for (let day = 1; day <= daysInMonth; day++) {
-      currentWeek.push(day);
+      const event = this.events.find(
+        (event) => event.date === this.getDateKey(day)
+      );
+      currentWeek.push({
+        day,
+        isPreviousMonth: false,
+        isNextMonth: false,
+        event,
+      });
       if (currentWeek.length === 7) {
         calendar.push(currentWeek);
         currentWeek = [];
       }
     }
 
-    // Add next month's days to fill the calendar
-    for (let i = lastDay; i < 6; i++) {
-      currentWeek.push();
-    }
-
     if (currentWeek.length > 0) {
+      let nextMonthDay = 1;
+      while (currentWeek.length < 7) {
+        currentWeek.push({
+          day: nextMonthDay,
+          isPreviousMonth: false,
+          isNextMonth: true,
+          event: undefined,
+        });
+        nextMonthDay++;
+      }
       calendar.push(currentWeek);
     }
 
     return calendar;
-  }
-
-  showEvents(dateKey: string) {
-    if (this.events[dateKey]) {
-      this.selectedEvent = this.events[dateKey][0];
-    }
-  }
-
-
-  closeEventDetails() {
-    this.selectedEvent = null;
-  }
-
-  changeMonth(monthIndex: number) {
-    this.selectedMonth = new Date(this.selectedMonth.getFullYear(), monthIndex, 1);
   }
 
   isToday(day: number): boolean {
@@ -102,4 +147,20 @@ export class AppComponent {
     );
   }
 
+  isDateInCurrentMonth(
+    day: number,
+    isPreviousMonth: boolean,
+    isNextMonth: boolean
+  ): boolean {
+    if (isPreviousMonth || isNextMonth) {
+      return false;
+    }
+
+    const selectedYear = this.selectedMonth.getFullYear();
+    const selectedMonth = this.selectedMonth.getMonth();
+
+    return (
+      new Date(selectedYear, selectedMonth, day).getMonth() === selectedMonth
+    );
+  }
 }
